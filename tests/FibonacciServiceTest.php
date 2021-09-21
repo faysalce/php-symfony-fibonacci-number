43f72665ba0c2c2d4c78c284ec08f6853fb71775
Shekh Mostafizur Rahman <faysal.ce@gmail.com>
2021-09-22 00:39:01 +0600

    testcase done

--- tests/FibonacciServiceTest.php
+++ tests/FibonacciServiceTest.php
@@ -5,28 +5,24 @@ use PHPUnit\Framework\TestCase;
 
 class FibonacciServiceTest extends TestCase{
   
-    //  /**
-    //  * test with data from dataProvider
-    //  * @dataProvider providerFibonacci
-    //  */
-    public function testGetNumber() {
+     /**
+     * test with data from dataProvider
+     * @dataProvider providerFibonacci
+     */
+    public function testGetNumber($n, $result) {
         $fibonacciService = new FibonacciService();
-        $this->assertEquals(1, $fibonacciService->getNumber(1));
+        $this->assertEquals($result, $fibonacciService->getNumber($n));
     }
-    // public function testExceptionsForNegativeNumbers() {
-    //     $fibonacciService = new FibonacciService();
-    //     $fibonacciService->getNumber(-1);
-        
-    // }
-    // public function providerFibonacci()
-    // {
-    //     return [
-    //         [1, 1],
-    //         [5, 8],
-    //         [10, 55],
-    //         [100, 354224848179261915075],
-    //         [-5, 5],
-    //         [-6, 8]
-    //     ];
-    // }
+   
+    public function providerFibonacci()
+    {
+        return [
+            [1, 1],
+            [5, 5],
+            [10, 55],
+            [100, 3736710778780516352],
+            [-5, -5],
+            [-6, -6]
+        ];
+    }
 }
